57da8b00745428abc5b7970a8fb02f081f2aea33
Kyle Spiller <me@kylespiller.com>
2025-02-28 18:37:56 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,22 +1,22 @@
 #! /usr/bin/bash
 
-
 # Define the files to back up
 BACKUP_FILES=(".env" "extra-hosts.yml")
-BACKUP_DIR="../__backup"
+BACKUP_DIR="__backup"
+APP_DIR="devtainer"
 
 # Ensure backup directory exists
 mkdir -p "$BACKUP_DIR"
 
 # Backup existing files
 for file in "${BACKUP_FILES[@]}"; do
-    if [ -f "$file" ]; then
+    if [ -f "$APP_DIR/$file" ]; then
         echo "Backing up $file..."
-        cp "$file" "$BACKUP_DIR/$file.bak"
+        cp "$APP_DIR/$file" "$BACKUP_DIR/$file.bak"
     fi
 done
 
-rm -rf devtainer
+rm -rf $APP_DIR
 
 echo "Updating services..."
 git clone https://github.com/KyleFS/docker-dev-infrastructure devtainer
@@ -24,13 +24,14 @@ git clone https://github.com/KyleFS/docker-dev-infrastructure devtainer
 # Restore backed-up files
 for file in "${BACKUP_FILES[@]}"; do
     if [ -f "$BACKUP_DIR/$file.bak" ]; then
-        rm -rf "$file"
+        rm -rf "$APP_DIR/$file"
         echo "Restoring $file..."
-        mv "$BACKUP_DIR/$file.bak" "$file"
+        mv "$BACKUP_DIR/$file.bak" "$APP_DIR/$file"
     fi
 done
 
-cp devtainer/update.sh update.sh
+rm -rf update.sh
+cp $APP_DIR/update.sh update.sh
 chmod +x update.sh
 
 # Delete backup folder
