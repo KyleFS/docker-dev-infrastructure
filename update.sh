d36ae61855078877135fe62feefc4afc8c6621a5
Kyle Spiller <me@kylespiller.com>
2025-02-28 17:36:11 -0500

    Update the updater

--- update.sh
+++ update.sh
@@ -24,10 +24,14 @@ git clone https://github.com/KyleFS/docker-dev-infrastructure devtainer
 # Restore backed-up files
 for file in "${BACKUP_FILES[@]}"; do
     if [ -f "$BACKUP_DIR/$file.bak" ]; then
+        rm -rf "$file"
         echo "Restoring $file..."
         mv "$BACKUP_DIR/$file.bak" "$file"
     fi
 done
+
+cp devtainer/update.sh update.sh
+chown +x update.sh
 
 # Delete backup folder
 rm -rf $BACKUP_DIR
