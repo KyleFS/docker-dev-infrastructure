944647b8f7e68d5da277c5b4c766a92ed73d2a0e
Kyle Spiller <me@kylespiller.com>
2025-02-28 17:28:38 -0500

    Add an update script

---
+++ update.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/bash
+
+
+# Define the files to back up
+BACKUP_FILES=(".env" "extra-hosts.yml")
+BACKUP_DIR="../__backup"
+
+# Ensure backup directory exists
+mkdir -p "$BACKUP_DIR"
+
+# Backup existing files
+for file in "${BACKUP_FILES[@]}"; do
+    if [ -f "$file" ]; then
+        echo "Backing up $file..."
+        cp "$file" "$BACKUP_DIR/$file.bak"
+    fi
+done
+
+rm -rf devtainer
+
+echo "Updating services..."
+git clone https://github.com/KyleFS/docker-dev-infrastructure devtainer
+
+# Restore backed-up files
+for file in "${BACKUP_FILES[@]}"; do
+    if [ -f "$BACKUP_DIR/$file.bak" ]; then
+        echo "Restoring $file..."
+        mv "$BACKUP_DIR/$file.bak" "$file"
+    fi
+done
+
+# Delete backup folder
+rm -rf $BACKUP_DIR
+
+# Deploy the docker compose stack.
+docker stack rm devtainer
+docker stack deploy -c devtainer/docker-compose.yml devtainer
+
+echo "Update process complete."
